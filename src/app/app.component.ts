5648e8645ea516a6c6231ee5742a2491e20791c2
Matt Hill <matthewonthemoon@gmail.com>
2020-04-12 09:42:51 -0600

    groundwork for background listener and local notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core'
 import { Platform } from '@ionic/angular'
 
-import { Plugins } from '@capacitor/core'
+import { Plugins, LocalNotifications } from '@capacitor/core'
 import { HttpService } from './http-service'
-const { SplashScreen } = Plugins
+import { HttpVerb } from 'capacitor-tor-client'
+const { SplashScreen, BackgroundTask } = Plugins
 
 @Component({
   selector: 'app-root',
@@ -19,14 +20,41 @@ export class AppComponent {
     this.initializeApp()
   }
 
-  initializeApp () {
+  initializeApp (): void {
     this.platform.ready().then(async () => {
       // init Tor process
       await this.httpService.initTor()
+      // add background listener for new messages (https://github.com/ionic-team/capacitor/issues/69)
+      // await BackgroundTask.background(() => {
+      //   this.listenForNotifications()
+      // })
       // dismiss splash screen
       setTimeout(() => {
         SplashScreen.hide()
       }, 300)
+    })
+  }
+
+  async listenForNotifications (): Promise<void> {
+    const messages = await this.httpService.request({
+      verb: HttpVerb.GET,
+      host: '<address>.onion',
+      path: '/notifications',
+      port: 5959,
+    })
+
+    const quantity = messages.length
+
+    if (!quantity) { return }
+
+    await LocalNotifications.schedule({
+      notifications: [
+        {
+          id: 1,
+          title: 'New Message',
+          body: `You have ${quantity} new messages.`,
+        },
+      ],
     })
   }
 }
